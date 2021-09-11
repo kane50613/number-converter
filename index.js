7f9f3ed0a5ffcca9286e89335ca0a2a1effef0ff
Gary50613 <50613gary@gmail.com>
2021-09-11 21:38:29 +0800

    add hex & binary prefix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const $ = mdui.$
-const numbers = [["十進位", "dec", 10], ["十六進位", "hex", 16], ["二進位", "bin", 2], ["八進位", "oct", 8]]
+const numbers = [["十進位", "dec", 10], ["十六進位", "hex", 16, "0x"], ["二進位", "bin", 2, "0b"], ["八進位", "oct", 8]]
 
 for(let num of numbers) {
     let div = document.createElement('div'),
@@ -11,21 +11,29 @@ for(let num of numbers) {
     h2.classList.add('title')
     h2.innerText = num[0]
     input.id = num[1]
+    input.value = num[3] ?? ''
 
     div.appendChild(h2)
     div.appendChild(input)
     $(`#containers`).get(0).appendChild(div)
 
-    document.getElementById(num[1]).addEventListener('input', () => {
+    const selector = $(`#${num[1]}`).get(0)
+
+    selector.addEventListener('input', () => {
+        if(num[3] && selector.value.slice(num[3].length).length <= 0)
+            return selector.value = num[3]
+
         for(let to_change of numbers) {
             if(num === to_change)
                 continue
 
-            const value = $(`#${num[1]}`).get(0).value
-            const target = parseFloat(value, num[2])
+            let value = selector.value
+            if(num[3])
+                 value = value.slice(num[3].length)
+            let target = parseFloat(value, num[2])
 
-            $(`#${to_change[1]}`).get(0).value = value.length === 0 ? '' :
-                (isNaN(target) || !isFinite(target) ? '錯誤' : target.toString(to_change[2])).toUpperCase()
+            $(`#${to_change[1]}`).get(0).value = (to_change[3] ?? '') + (value.length === 0 ? '' :
+                (isNaN(target) || !isFinite(target) ? '錯誤' : target.toString(to_change[2]))).toUpperCase()
         }
     })
 }
